1820cf9ed3e85ec434632e9126110dfd5deb5981
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-01-08 17:56:13 +0530

    updates:

--- frontend/src/pages/student/Course.jsx
+++ frontend/src/pages/student/Course.jsx
@@ -8,7 +8,7 @@ const Course = ({course}) => {
   return (
     <Link to ={`/course-details/${course._id}`} >
       <Card
-        className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
+        className="w-fit overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
       >
         <div className="relative">
           <img
